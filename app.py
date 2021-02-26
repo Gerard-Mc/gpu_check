acb62987e6b4d44437b4f439aedadd5230e88970
Gerard-Mc <gerardmcdaid88@gmail.com>
2021-02-26 22:42:18 +0000

    remove debug from app.py

--- app.py
+++ app.py
@@ -309,5 +309,5 @@ def submit():
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+            port=int(os.environ.get("PORT")))
+           
